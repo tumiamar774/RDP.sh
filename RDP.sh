62890d1b509c2cdaa5068ef0706c63f93eb036a2
tumiamar774 <80454219+tumiamar774@users.noreply.github.com>
2021-03-21 08:53:01 +0600

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Check Out My Name">&2
 {
-sudo useradd -m MOIN
-sudo useradd MOIN sudo
-echo 'MOIN:8426' | sudo chpasswd
+sudo useradd -m ALOK
+sudo useradd ALOK sudo
+echo 'ALOK:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt_get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -19,12 +19,12 @@ sudo apt install --assum-yes --fix-broken
 sudo apt install nautilus nano -y
 sudo apt -y install obs-studio
 sudo apt -y install firefox
-sudo adduser MOIN chrome-remote-desktop
+sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complited " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless Copy Command Of Debian Linux And Peste Down\n'
 read -p "Peste Here: " CRP
-su - MOIN -c """$CRP"""
+su - ALOK -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 print 'Your Sudo Passwd Is 8426 \n'
